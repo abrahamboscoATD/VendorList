d555efdbe60f8e048405124759d7f64b488e725f
Abraham Bosco <abrahambosco@Abrahams-MacBook-Air.local>
2024-07-24 12:08:45 +0400

    Upgraded Node.js version and fixed package.json

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -3,24 +3,21 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [vitePreprocess({})],
+  preprocess: [vitePreprocess({})],
 
-	kit: {
-		// Use Node adapter for deployment
-		adapter: adapter(),
-		alias: {
-			'@': './src',
-			'@/*': './src/*'
-		},
-		files: {
-			hooks: {
-				client: 'src/lib/config/hooks.client.ts',
-				server: 'src/lib/config/hooks.server.ts'
-			}
-		}
-	}
+  kit: {
+    adapter: adapter(),
+    alias: {
+      '@': './src',
+      '@/*': './src/*'
+    },
+    files: {
+      hooks: {
+        client: 'src/lib/config/hooks.client.ts',
+        server: 'src/lib/config/hooks.server.ts'
+      }
+    }
+  }
 };
 
 export default config;
