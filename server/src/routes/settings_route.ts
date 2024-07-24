32b503cf56fa20e1228d540719acd85332ed3fe8
Abraham Bosco <abrahambosco@Abrahams-MacBook-Air.local>
2024-07-24 12:23:25 +0400

    Added handler functions to routes in settings_route.ts

--- server/src/routes/settings_route.ts
+++ server/src/routes/settings_route.ts
@@ -5,15 +5,34 @@ export default function auth_router(fastify: FastifyInstance, opt: FastifyPlugin
     |-------> Profile Routes <---------|
   */
 
-  fastify.put("/profile/update-user/:user_id");
-  fastify.put("/profile/change-password/:user_id");
+  fastify.put("/profile/update-user/:user_id", async (request, reply) => {
+    // Add your handler logic here
+    reply.send({ status: 'User profile updated' });
+  });
+
+  fastify.put("/profile/change-password/:user_id", async (request, reply) => {
+    // Add your handler logic here
+    reply.send({ status: 'Password changed' });
+  });
 
   /*
     |-------> USER Routes <---------|
   */
-  fastify.get("/user/get-all");
-  fastify.delete("/user/delete/:user_id");
-  fastify.put("/user/change-role/:user_id");
+  
+  fastify.get("/user/get-all", async (request, reply) => {
+    // Add your handler logic here
+    reply.send({ users: [] });
+  });
+
+  fastify.delete("/user/delete/:user_id", async (request, reply) => {
+    // Add your handler logic here
+    reply.send({ status: 'User deleted' });
+  });
+
+  fastify.put("/user/change-role/:user_id", async (request, reply) => {
+    // Add your handler logic here
+    reply.send({ status: 'User role changed' });
+  });
 
   done();
 }
