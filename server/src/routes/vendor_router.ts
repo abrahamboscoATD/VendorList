bfbf40e4bd353ac225e490ce63a2d39482db265f
Abraham Bosco <abrahambosco@Abrahams-MacBook-Air.local>
2024-07-24 14:13:00 +0400

    Updated vendor_router.ts and vendor.ts to ensure type compatibility with Fastify

--- server/src/routes/vendor_router.ts
+++ server/src/routes/vendor_router.ts
@@ -1,10 +1,14 @@
-import { FastifyInstance, FastifyPluginOptions } from "fastify";
+import { FastifyInstance } from "fastify";
 import { create_vendor_controller, delete_one_vendor_controller, get_all_vendor_controller, update_vendor_controller } from "../controller/vendor.js";
 
-export default function vendor_router(fastify: FastifyInstance, opt: FastifyPluginOptions, done: (err?: Error) => void) {
+async function vendorRouter(fastify: FastifyInstance) {
   fastify.get("/get-all", get_all_vendor_controller);
+
   fastify.post("/create-one", create_vendor_controller);
-  fastify.put("/update-one/:vendorId", update_vendor_controller);
-  fastify.delete("/delete-one/:vendorId", delete_one_vendor_controller);
-  done();
+
+  fastify.put<{ Params: { vendorId: string } }>("/update-one/:vendorId", update_vendor_controller);
+
+  fastify.delete<{ Params: { vendorId: string } }>("/delete-one/:vendorId", delete_one_vendor_controller);
 }
+
+export default vendorRouter;
